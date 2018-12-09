60d3b1191d459945a73186690a3887b40a383d9a
Jorge <santos.jorge@scandservices.com>
2018-12-09 19:00:10 +0100

    modified HelloWorld app

--- src/Basics/HelloWorld.java
+++ src/Basics/HelloWorld.java
@@ -3,6 +3,7 @@ package Basics;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("This is it!");
+		System.out.println("Setting up GitHub for the second time");
 
 }
 }
